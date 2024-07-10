04f3d508000c2cdf7dbd84be769d8c3541eb3af9
OliwierSellig <oliwierandrzej.sellig@gmail.com>
2024-07-10 14:49:29 +0200

    feat: add revalidate path

---
+++ next/app/api/revalidate/route.ts
@@ -0,0 +1,50 @@
+import { isValidSignature, SIGNATURE_HEADER_NAME } from "@sanity/webhook";
+import type { NextApiRequest, NextApiResponse } from "next";
+
+type Data = {
+  message: string;
+};
+
+export const config = {
+  api: {
+    bodyParser: false,
+  },
+};
+
+const secret = process.env.SANITY_REVALIDATE_SECRET;
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<Data>,
+) {
+  const signature = req.headers[SIGNATURE_HEADER_NAME] as string;
+  const body = await readBody(req); // Read the body into a string
+
+  if (req.method !== "POST") {
+    return res.status(401).json({ message: "Must be a POST request" });
+  }
+
+  if (!isValidSignature(body, signature, secret!)) {
+    res.status(401).json({ message: "Invalid signature" });
+    return;
+  }
+
+  try {
+    const { _type: type, slug } = JSON.parse(body);
+
+    await res.revalidate(`/`); // The landing page featured projects
+    return res.json({
+      message: `Revalidated "${type}" with slug "${slug.current}"`,
+    });
+  } catch (err) {
+    return res.status(500).send({ message: "Error revalidating" });
+  }
+}
+
+async function readBody(readable: any) {
+  const chunks = [];
+  for await (const chunk of readable) {
+    chunks.push(typeof chunk === "string" ? Buffer.from(chunk) : chunk);
+  }
+  return Buffer.concat(chunks).toString("utf8");
+}
